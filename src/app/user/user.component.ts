d51c5acb0a1df171c5caaf5faa11d4e5ebd34e16
Azure-dvl <subarachisekai@gmail.com>
2024-06-02 11:00:36 -0400

    Lalala

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
 
 import { User } from '../interfaces';
 import { list_User } from '../list';
@@ -6,10 +7,24 @@ import { list_User } from '../list';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
   userl = list_User;
+
+  formadd = false;
+  listuser = false;
+
+  Mostrar_list():void{
+    this.listuser = true;
+  }
+
+  Mostrar_form():void{
+    this.formadd = true;
+  }
 }
